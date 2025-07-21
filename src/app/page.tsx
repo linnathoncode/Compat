8a8b74e49345e8bf684afa994c4c2d86e2514427
furkangocuk <furkangocuk42@gmail.com>
2025-07-21 20:53:28 +0300

    add: landing page and dashboard, feat: route according to the state of auth

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,22 @@
-import TextField from "~/components/TextField";
+"use client";
 
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
+import { useAuth } from "~/context/AuthContext";
 export default function HomePage() {
+  const router = useRouter();
+  const { status } = useAuth();
+
+  useEffect(() => {
+    if (status === "authenticated") {
+      router.push("/dashboard");
+    }
+  }, [router, status]);
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-start bg-gradient-to-b from-[#000000f9] to-[#1e152c] pt-12 text-white">
-      <TextField />
+      {/**TO:DO Landing Page */}
+      Landing Page
     </main>
   );
 }
